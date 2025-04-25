83c57832b18cb42c1c1f57eeb2c804c5cb4b8492
matza <mzamunerm@gmail.com>
2025-04-25 14:45:23 -0300

    Added: items

--- app/services/dungeon_service.py
+++ app/services/dungeon_service.py
@@ -5,9 +5,9 @@ from app.utils.schema_util import load_json_schema
 client = OpenAI()
 
 
-def generate_dungeon(layout: dict):
+def generate_dungeon():
     dungeon_system = "You are an imaginative Dungeon Master."
-    dungeon_user = f"Populate each dungeon room based on this layout : {layout}"
+    dungeon_user = "Populate this dungeon room based on the dungeon theme."
     dungeon_schema = load_json_schema("dungeon_schema.json")
     response = client.responses.create(
         model="gpt-4.1-nano",
