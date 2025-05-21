a71ed91d101c5149b92c189e9711ac3d04942568
matza <mzamunerm@gmail.com>
2025-05-20 21:38:42 -0300

    Changed story route

--- app/routes/story_route.py
+++ app/routes/story_route.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, request, jsonify
 from app.services.story_service import generate_story
 
-story_blueprint = Blueprint("story", __name__)
+story_blueprint = Blueprint("continue_story", __name__)
 
 
 @story_blueprint.route("/", methods=["POST"])
