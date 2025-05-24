1f98629a0a80df47aa52ece31912aae28744928f
matza <mzamunerm@gmail.com>
2025-05-23 21:27:02 -0300

    Modified: dungeon layout

--- app/services/dungeon_room_service.py
+++ app/services/dungeon_room_service.py
@@ -5,16 +5,16 @@ from app.utils.schema_util import load_json_schema
 client = OpenAI()
 
 
-def generate_dungeon_layout():
-    dungeon_layout_system = "You are an imaginative Dungeon Master."
-    dungeon_layout_user = "Generate this dungeon layout based on the dungeon theme, the dungeon layout has a 50% chance to not have any enemies present, 25% chance to have 1 enemy present, 12.5% chance to have 2 enemies present and 12.5% chance to have 3 enemies present."
-    dungeon_layout_schema = load_json_schema("dungeon_layout_schema.json")
+def generate_dungeon_room():
+    dungeon_room_system = "You are an imaginative Dungeon Master."
+    dungeon_room_user = "Generate this dungeon room based on the dungeon theme, the dungeon room has a 50% chance to not have any enemies present, 25% chance to have 1 enemy present, 12.5% chance to have 2 enemies present and 12.5% chance to have 3 enemies present."
+    dungeon_room_schema = load_json_schema("dungeon_room_schema.json")
     response = client.responses.create(
         model="gpt-4.1-nano",
         input=[
-            {"role": "system", "content": dungeon_layout_system},
-            {"role": "user", "content": dungeon_layout_user},
+            {"role": "system", "content": dungeon_room_system},
+            {"role": "user", "content": dungeon_room_user},
         ],
-        text=dungeon_layout_schema,
+        text=dungeon_room_schema,
     )
     return response.output_text
