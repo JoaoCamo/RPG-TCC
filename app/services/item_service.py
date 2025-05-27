c11871c13939202b5c70e1650c3c9c655e5d3bae
matza <mzamunerm@gmail.com>
2025-05-26 22:27:06 -0300

    Modified: item

--- app/services/item_service.py
+++ app/services/item_service.py
@@ -8,13 +8,13 @@ client = OpenAI()
 def generate_item(character):
     item_system = "You are an imaginative Dungeon Master."
     item_user = f"Generate an item, either a weapon or armor, based on the character that will drop it, the item should be corresponding to the character.\nCharacter: {character}"
-    item_room_schema = load_json_schema("item_schema.json")
+    item_schema = load_json_schema("item_schema.json")
     response = client.responses.create(
         model="gpt-4.1-nano",
         input=[
             {"role": "system", "content": item_system},
             {"role": "user", "content": item_user},
         ],
-        text=item_room_schema,
+        text=item_schema,
     )
     return response.output_text
